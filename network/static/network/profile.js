be9e1615108faeca632ecbf4e5b8ffe21e9eac4a
Balaji <balajikaredla12345@gmail.com>
2022-05-26 18:16:18 +0530

    Editing1

--- network/static/network/profile.js
+++ network/static/network/profile.js
@@ -28,104 +28,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 })
 
-// load_posts = (posts, num) => {
-//     posts.innerHTML = ''
-//     username1 = document.querySelector('h4').innerHTML
-//     console.log(num)
-//     fetch(`/profile-api/${username1}/${num}`)
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data.posts)
-//         data.posts.forEach(post => {
-//             console.log(post)
-//             const div = document.createElement('div')
-//             div.className = 'post-div'
-//             const b = document.createElement('b')
-//             const a1 = document.createElement('a')
-//             a1.innerHTML = post.poster
-//             a1.className = 'profile-link'
-//             a1.href = `/profile/${post.poster}`
-//             b.append(a1)
-//             div.append(b)
-//             div.innerHTML += `<div id="content-${post.id}">${post.content}</div>${post.timestamp}<br><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-suit-heart-fill" viewBox="0 0 16 16"><path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"/></svg> ${post.likes}`
-//             div.style.marginRight = '0'
-//             div.style.marginLeft = '0'
-//             posts.append(div)
-//             if (username === post.poster) {
-//                 console.log(post.poster)
-//                 const a2 = document.createElement('button')
-//                 a2.className = 'btn btn-outline-primary'
-//                 a2.innerHTML = 'Edit'
-//                 a2.addEventListener('click', () => {
-//                     const text = document.createElement('textarea')
-//                     text.id = `content${post.id}`
-//                     text.className = 'form-control'
-//                     text.innerHTML = post.content
-//                     const save = document.createElement('button')
-//                     save.className = 'btn btn-outline-primary'
-//                     save.innerHTML = 'Save'
-//                     save.onclick = () => {
-//                         const content = document.querySelector(`#content${post.id}`).value
-//                         fetch('/new-post', {
-//                             method: 'POST',
-//                             body: JSON.stringify({
-//                                 content: content
-//                             })
-//                         })
-//                         .then(response => response.json())
-//                         .then(result => {
-//                             console.log(result);
-//                         });
-//                         a2.style.display = 'block'
-//                         document.querySelector(`#content-${post.id}`).innerHTML = content
-//                     }
-//                     document.querySelector(`#content-${post.id}`).innerHTML = '<br>'
-//                     document.querySelector(`#content-${post.id}`).append(text)
-//                     document.querySelector(`#content-${post.id}`).innerHTML += '<br>'
-//                     document.querySelector(`#content-${post.id}`).append(save)
-//                     a2.style.display = 'none'
-//                 })
-//                 div.innerHTML += '<br>'
-//                 div.append(a2)
-//             }
-//         })
-//         if (!data.previous && !data.next) {
-//             document.querySelector('#nav').style.display = 'none'
-//         } else if (!data.previous) {
-//             document.querySelector('#nav').style.display = 'block'
-//             document.querySelector('#previous').style.display = 'none'
-//             document.querySelector('#next').style.display = 'block'
-//             document.querySelector('#next-button').addEventListener('click', () => {
-//                 posts.innerHTML = ''
-//                 load_posts(posts, num+1)
-//                 posts.innerHTML = ''
-//             })
-//         } else if (!data.next) {
-//             document.querySelector('#nav').style.display = 'block'
-//             document.querySelector('#previous').style.display = 'block'
-//             document.querySelector('#next').style.display = 'none'
-//             document.querySelector('#previous-button').addEventListener('click', () => {
-//                 posts.innerHTML = ''
-//                 load_posts(posts, num-1)
-//                 posts.innerHTML = ''
-//             })
-//         } else {
-//             document.querySelector('#previous').style.display = 'block'
-//             document.querySelector('#nav').style.display = 'block'
-//             document.querySelector('#next-button').addEventListener('click', () => {
-//                 posts.innerHTML = ''
-//                 load_posts(posts, num+1)
-//                 posts.innerHTML = ''
-//             })
-//             document.querySelector('#previous-button').addEventListener('click', () => {
-//                 posts.innerHTML = ''
-//                 load_posts(posts, num-1)
-//                 posts.innerHTML = ''
-//             })
-//         }
-//     })
-// }
-
 load_posts = (posts, num, username) => {
     posts.innerHTML = ''
     username1 = document.querySelector('h4').innerHTML
@@ -143,17 +45,17 @@ load_posts = (posts, num, username) => {
             a1.href = `/profile/${post.poster}`
             b.append(a1)
             div.append(b)
-            div.innerHTML += `<div id="content-${post.id}">${post.content}</div>${post.timestamp}<br><svg id="post-like-${post.id}" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-suit-heart-fill" viewBox="0 0 16 16"><path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"/></svg> ${post.likes}<br>`
+            div.innerHTML += `<div id="content-${post.id}">${post.content}</div>${post.timestamp}<br><svg id="post-like-${post.id}" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-suit-heart-fill" viewBox="0 0 16 16"><path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"/></svg> `
             document.querySelector('#posts').append(div)
-            document.querySelector(`#post-like-${post.id}`).addEventListener('click', () => {
-                fetch(`posts/like/${post.id}`, {
-                    method: 'PUT',
-                    body: JSON.stringify({
-                        like: 1
-                    })
-                })
-            })
-            console.log(username)
+            // document.querySelector(`#post-like-${post.id}`).addEventListener('click', () => {
+            //     fetch(`posts/like/${post.id}`, {
+            //         method: 'PUT',
+            //         body: JSON.stringify({
+            //             like: 1
+            //         })
+            //     })
+            // })
+            // console.log(username)
             if (username === post.poster) {
                 const a2 = document.createElement('button')
                 a2.className = 'btn btn-outline-primary'
@@ -187,12 +89,13 @@ load_posts = (posts, num, username) => {
                     document.querySelector(`#content-${post.id}`).append(save)
                     a2.style.display = 'none'
                 })
-                div.innerHTML += '<br>'
+                div.innerHTML += `${post.likes}<br><br>`
                 div.append(a2)
             } else {
                 const like = document.createElement('button')
                 console.log(post.id)
                 console.log(post.liked)
+                div.innerHTML +=   `${post.likes}<br>`
                 if(post.liked === 1){
                     like.className = 'btn btn-primary'
                     like.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-down-fill" viewBox="0 0 16 16"><path d="M6.956 14.534c.065.936.952 1.659 1.908 1.42l.261-.065a1.378 1.378 0 0 0 1.012-.965c.22-.816.533-2.512.062-4.51.136.02.285.037.443.051.713.065 1.669.071 2.516-.211.518-.173.994-.68 1.2-1.272a1.896 1.896 0 0 0-.234-1.734c.058-.118.103-.242.138-.362.077-.27.113-.568.113-.856 0-.29-.036-.586-.113-.857a2.094 2.094 0 0 0-.16-.403c.169-.387.107-.82-.003-1.149a3.162 3.162 0 0 0-.488-.9c.054-.153.076-.313.076-.465a1.86 1.86 0 0 0-.253-.912C13.1.757 12.437.28 11.5.28H8c-.605 0-1.07.08-1.466.217a4.823 4.823 0 0 0-.97.485l-.048.029c-.504.308-.999.61-2.068.723C2.682 1.815 2 2.434 2 3.279v4c0 .851.685 1.433 1.357 1.616.849.232 1.574.787 2.132 1.41.56.626.914 1.28 1.039 1.638.199.575.356 1.54.428 2.591z"></path></svg>Dislike'
@@ -207,8 +110,14 @@ load_posts = (posts, num, username) => {
                             like: 1
                         })
                     })
-
-                    load_posts(num)
+                    if(post.liked === 1){
+                        div.innerHTML -= `${post.likes}<br>`
+                        div.innerHTML += `${post.likes - 1}<br>`
+                    } else {
+                        div.innerHTML -= `${post.likes}<br>`
+                        div.innerHTML += `${post.likes + 1}<br>`
+                    }
+                    // load_posts(num)
                 })
                 div.append(like)
             }
@@ -280,3 +189,7 @@ load_data = (username) => {
         }
     })
 }
+
+// like = {
+    
+// }
