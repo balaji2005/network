8dd2c427c4c6112c2a6e5ba10e17cd7bc42d996a
Balaji <balajikaredla12345@gmail.com>
2022-05-27 12:36:39 +0530

    Some corrections

--- network/views.py
+++ network/views.py
@@ -125,7 +125,7 @@ def posts(request, username, num):
 
 # API Route to get Profile of a user
 @csrf_exempt
-def profile(request, user_username, poster_username, num):
+def profile(request, username, num):
     username = username.strip()
     user = User.objects.get(username = username)
     print(user.follower.all(), 'getting data')
