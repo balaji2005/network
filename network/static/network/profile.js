0e2e64bb23f0410c076c49d539fa893c20d42deb
Balaji <balajikaredla12345@gmail.com>
2022-05-27 11:57:41 +0530

    Small bugs

--- network/static/network/profile.js
+++ network/static/network/profile.js
@@ -45,7 +45,7 @@ load_posts = (posts, num, username) => {
             a1.href = `/profile/${post.poster}`
             b.append(a1)
             div.append(b)
-            div.innerHTML += `<div id="content-${post.id}">${post.content}</div>${post.timestamp}<br><svg id="post-like-${post.id}" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-suit-heart-fill" viewBox="0 0 16 16"><path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"/></svg> ${post.likes}`
+            div.innerHTML += `<div id="content-${post.id}">${post.content}</div>${post.timestamp}<br><svg id="post-like-${post.id}" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-suit-heart-fill" viewBox="0 0 16 16"><path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"/></svg> `
             document.querySelector('#posts').append(div)
             if (username === post.poster) {
                 const a2 = document.createElement('button')
@@ -86,6 +86,7 @@ load_posts = (posts, num, username) => {
                 const like = document.createElement('button')
                 // console.log(post.id)
                 // console.log(post.liked)
+                like.id = 'like-button'
                 console.log(div.innerHTML)
                 div.innerHTML +=   `${post.likes}<br>`
                 console.log(div.innerHTML)
