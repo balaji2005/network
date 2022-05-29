4770563bcc482825628d24f2746a1bdec0aaafe5
Balaji <balajikaredla12345@gmail.com>
2022-05-29 17:20:24 +0530

    Some Corrections

--- network/static/network/profile.js
+++ network/static/network/profile.js
@@ -63,7 +63,7 @@ load_posts = (posts_div, num, username) => {
             post_div.append(b)
             post_div.innerHTML += `<div id="content-${post.id}">${post.content}</div>${post.timestamp}<br><svg id="post-like-${post.id}" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-suit-heart-fill" viewBox="0 0 16 16"><path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"/></svg> `
             posts_div.append(post_div)
-            if (username === post.poster) {
+            if (user_username === post.poster) {
                 const a2 = document.createElement('button')
                 a2.className = 'btn btn-outline-primary'
                 a2.innerHTML = 'Edit'
